fa78917ddc98740f25c2422d9acdc1f3afe5b3bf
Kaleb Boothe <kboothe617@gmail.com>
2024-06-22 13:34:10 -0600

    random computer input

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,12 +1,24 @@
-console.log("hit");
+// console.log("hit");
 // computer makes a choice
 confirm("you wanna play rps"); // yes or no
 prompt("rock, paper or scissors"); // user input
 // what if the user inputs anything else that you don't expect
-alert("hi"); //
+// alert("hi"); //
 // you make a choice
 // compare the choices
+const computerChoice = () => {
+    const number = Math.floor(Math.random() * 3);
+    switch (number) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    }
+}
 
+console.log(computerChoice());
 // scenarios for different coices by user & computer
 // if computer wins, mark it as a win for it
 // ask the user if they wanna play again
