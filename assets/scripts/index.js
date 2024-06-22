27ea3b08c259bc29b14b1273889a05a50c7582f6
Victor Roman <Romantech91>
2024-06-22 12:14:03 -0700

    added notes

---
+++ assets/scripts/index.js
@@ -0,0 +1,15 @@
+console.log("hit");
+// computer makes a choice
+confirm("you wanna play rps"); // yes or no
+prompt("rock, paper or scissors"); // user input
+// what if the user inputs anything else that you don't expect
+alert("hi"); //
+// you make a choice
+// compare the choices
+
+// scenarios for different coices by user & computer
+// if computer wins, mark it as a win for it
+// ask the user if they wanna play again
+// play again if yes
+// exit the app if no
+// show final results
